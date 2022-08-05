295c4d5d18b9914b9858d656f1fa7f7e8f040847
Mohab Mamdouh <mohabmamdouh22@gmail.com>
2022-08-05 23:51:20 +0200

    m

--- resources/views/Admin/layouts/includes/navbar.blade.php
+++ resources/views/Admin/layouts/includes/navbar.blade.php
@@ -14,9 +14,6 @@
                             {{ __('Portfolio') }}
                         </a>
                     </li>
-                    {{-- <li class="">
-                        <a href="{{ route('portfolio') }}" class="">Hello</a>
-                    </li> --}}
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             <img src="{{ asset('storage/users/'.$user->profileImage) }}" alt="MOHAB" style="width: 40px;height: 40px;border-radius: 100%" >
